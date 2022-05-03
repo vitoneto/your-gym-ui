7f8372729e4c03f9c0863631f3bc008abbc7fc8b
Vito Neto <vitomanoelneto1997@gmail.com>
2022-05-03 16:54:11 -0300

    Quality life section started

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    cards = [
+        { icon: '', label: 'Saúde' },
+        { icon: '', label: 'Massa muscular' },
+        { icon: '', label: 'Força' }
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
